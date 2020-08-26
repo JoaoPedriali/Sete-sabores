1b50b536d82c398018df95160945a8d6f0f79e96
Joao Pedro <joao.araujo@tegra.com.br>
2020-08-25 22:39:40 -0300

    Initial commit

---
+++ app/src/main/java/com/example/setesabores/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.setesabores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //João Pedro Pedriali de Araujo - RA 0097894 - Análise e desenvolvimento de sistemas
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
